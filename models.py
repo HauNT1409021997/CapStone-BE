c1f3468597e5996dd4bc9c7989779d6b8a54cf9c
Nguyễn Trọng Hậu <akitosama12546@gmail.com>
2022-09-10 09:44:05 +0700

    cst 7 commit

--- models.py
+++ models.py
@@ -2,7 +2,7 @@ from sqlalchemy import Column, ForeignKey, String, Integer, Float, Boolean
 from flask_sqlalchemy import SQLAlchemy
 database_name = "TODO"
 database_path = "postgresql://{}:{}@{}/{}".format('postgres', '123', 'localhost:5432', database_name)
-database_path = "postgresql://cmsrraognpmaqi:413c93c2cabff045b2bb6910aa152d4f611d582cffa2f1652701518a8d4f1189@ec2-107-23-76-12.compute-1.amazonaws.com:5432/dehkajpu6edohp"
+database_path = "postgresql+psycopg2://cmsrraognpmaqi:413c93c2cabff045b2bb6910aa152d4f611d582cffa2f1652701518a8d4f1189@ec2-107-23-76-12.compute-1.amazonaws.com:5432/dehkajpu6edohp?sslmode=require"
 db = SQLAlchemy()
 
 def setup_db(app, database_path=database_path):
